33e36fcbe463ff0c6153b4d4005eef234d085adb
eriskjel <eriskjel@stud.ntnu.no>
2023-02-19 12:04:51 +0100

    initial commit

---
+++ src/views/ContactFormView.vue
@@ -0,0 +1,14 @@
+<template>
+  <ContactForm></ContactForm>
+</template>
+
+<script>
+import ContactForm from '@/components/ContactForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ContactForm
+  }
+}
+</script>
